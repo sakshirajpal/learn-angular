558cbe56636e3eb9ee644c2bc4026497d647cdb2
srajpal <srajpal@3clogic.com>
2021-07-24 15:29:45 +0530

    structure creation

---
+++ src/app/hotel-listing/hotel-listing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HotelListingRoutingModule } from './hotel-listing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HotelListingRoutingModule
+  ]
+})
+export class HotelListingModule { }
